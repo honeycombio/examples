9e32cd5df15f2b4cf7bb8f8bc256a0992e0065e8
Shane Becker <veganstraightedge@gmail.com>
2018-09-22 17:04:31 -0700

    Rubocop autofix ruby-wiki-tracing

--- ruby-wiki-tracing/wiki.rb
+++ ruby-wiki-tracing/wiki.rb
@@ -1,5 +1,5 @@
-require "sinatra/base"
-require "libhoney"
+require 'sinatra/base'
+require 'libhoney'
 
 # Page represents the data (and some basic operations) on a wiki page.
 #
@@ -21,7 +21,7 @@ class Page
   def save(body)
     File.write(@filename, body)
     true
-  rescue => e
+  rescue StandardError => e
     false
   end
 end
@@ -36,15 +36,16 @@ end
 # This middleware treats each HTTP request as a distinct "trace." Each trace
 # begins with a top-level ("root") span indicating that the HTTP request has
 # begun.
-VALID_PATH = Regexp.new("^/(edit|save|view)/")
+VALID_PATH = Regexp.new('^/(edit|save|view)/')
 class RequestTracer
   def initialize(app)
     @app = app
   end
+
   def call(env)
     Thread.current[:request_id] = new_id
-    m = env["REQUEST_PATH"].match(VALID_PATH)
-    @app.with_span(m ? m[1] : env["REQUEST_PATH"]) do
+    m = env['REQUEST_PATH'].match(VALID_PATH)
+    @app.with_span(m ? m[1] : env['REQUEST_PATH']) do
       @app.call(env)
     end
   end
@@ -60,26 +61,25 @@ class App < Sinatra::Base
   # an environment variable.
   configure do
     set :libhoney, Libhoney::Client.new(
-      writekey: ENV["HONEYCOMB_WRITEKEY"],
-      dataset: "ruby-wiki-tracing-example"
+      writekey: ENV['HONEYCOMB_WRITEKEY'],
+      dataset: 'ruby-wiki-tracing-example'
     )
   end
 
   # Redirect to a default wiki page.
-  get "/" do
-    redirect "/view/Index"
+  get '/' do
+    redirect '/view/Index'
   end
 
   # Our "View" handler. Tries to load a page from disk and render it. Falls back
   # to the Edit handler if the page does not yet exist.
-  get "/view/:title" do |title|
-    @page = with_span("load_page", { title: title }) do
+  get '/view/:title' do |title|
+    @page = with_span('load_page', title: title) do
       load_page(title)
     end
-    if @page.nil?
-      return redirect "/edit/#{title}"
-    end
-    with_span("render_template", { template: "view" }) do
+    return redirect "/edit/#{title}" if @page.nil?
+
+    with_span('render_template', template: 'view') do
       erb(:view)
     end
   end
@@ -87,29 +87,26 @@ class App < Sinatra::Base
   # Our "Edit" handler. Tries to load a page from disk to seed the edit screen,
   # then renders a form to allow the user to define the content of the requested
   # wiki page.
-  get "/edit/:title" do |title|
-    @page = with_span("load_page", { title: title }) do
+  get '/edit/:title' do |title|
+    @page = with_span('load_page', title: title) do
       load_page(title)
     end
-    if @page.nil?
-      @page = Page.new(title)
-    end
+    @page = Page.new(title) if @page.nil?
 
-    with_span("render_template", { template: "edit" }) do
+    with_span('render_template', template: 'edit') do
       erb(:edit)
     end
   end
 
   # Our "Save" handler simply persists a page to disk.
-  post "/save/:page" do |title|
-    saved = with_span("File.write", { title: title, body_len: params["body"].size }) do
+  post '/save/:page' do |title|
+    saved = with_span('File.write', title: title, body_len: params['body'].size) do
       p = Page.new(title)
-      p.save(params["body"])
+      p.save(params['body'])
     end
-    if saved
-      return redirect "/view/#{title}"
-    end
-    "error"
+    return redirect "/view/#{title}" if saved
+
+    'error'
   end
 
   # This wrapper takes a span name, some optional metadata, and a block; then
@@ -119,7 +116,7 @@ class App < Sinatra::Base
   # The special sauce in this method is the definition / resetting of thread
   # local variables in order to correctly propagate "parent" identifiers down
   # into the block.
-  def with_span(name, metadata=nil)
+  def with_span(name, metadata = nil)
     id = new_id
     start = Time.new
     # Field keys to trigger Honeycomb's tracing functionality on this dataset
@@ -129,24 +126,20 @@ class App < Sinatra::Base
       name: name,
       id: id,
       traceId: Thread.current[:request_id],
-      serviceName: "wiki",
+      serviceName: 'wiki'
     }
     # Capture the calling scope's span ID, then restore it at the end of the
     # method.
     parent_id = Thread.current[:span_id]
-    if parent_id
-      data[:parentId] = parent_id
-    end
+    data[:parentId] = parent_id if parent_id
 
     # Set the current span ID before invoking the provided block, then capture
     # the return value to return after emitting the Honeycomb event.
     Thread.current[:span_id] = id
     ret = yield
 
-    data[:durationMs] = (Time.new - start)*1000
-    if metadata
-      data.merge!(metadata)
-    end
+    data[:durationMs] = (Time.new - start) * 1000
+    data.merge!(metadata) if metadata
 
     ev = settings.libhoney.event
     # NOTE: Don't forget to set the timestamp to `start` -- because spans are
@@ -162,13 +155,14 @@ class App < Sinatra::Base
     Thread.current[:span_id] = parent_id
   end
 
- private
+  private
 
   # Helper method for returning a Page object for easy rendering
   def load_page(title)
     p = Page.new(title)
-    return nil if !p.exist?
-    with_span("File.read") do
+    return nil unless p.exist?
+
+    with_span('File.read') do
       p.body = File.read(p.filename)
       p
     end
