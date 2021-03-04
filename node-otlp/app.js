fa3bddc87a6b44655566283b9ccf78bbb5961eee
Robb Kidd <robbkidd@honeycomb.io>
2021-03-04 08:03:23 -0500

    Fix no spans being generated or sent from node-otlp example (#55)
    * add plugin-http and update OTel packages to current versions
    
    * include a "my business logic" example span

--- node-otlp/app.js
+++ node-otlp/app.js
@@ -1,11 +1,15 @@
 "use strict";
+const opentelemetry = require('@opentelemetry/api');
 
 const PORT = process.env.PORT || "8080";
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+  const span = opentelemetry.trace.getTracer('default').startSpan('world-greeter');
+  console.log("Saying hello to the world.")
   res.send("Hello world!");
+  span.end();
 });
 
 app.listen(parseInt(PORT, 10), () => {
