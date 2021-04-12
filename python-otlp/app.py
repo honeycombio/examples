b3b8ae5dc8a419cfc4ce4d7bd27d1b47e327a6b9
shelby spees <shelbyspees@users.noreply.github.com>
2021-04-12 14:15:02 -0700

    [python-otlp] Update package versions and use latest interface (#57)

--- python-otlp/app.py
+++ python-otlp/app.py
@@ -1,25 +1,24 @@
 import flask
 import requests
-
 from grpc import ssl_channel_credentials
-
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
+    OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 otlp_exporter = OTLPSpanExporter(
 	endpoint="api.honeycomb.io:443",
 	credentials=ssl_channel_credentials(),
-	headers=(("x-honeycomb-team", ""),("x-honeycomb-dataset","python-otlp"))
+    headers=(("x-honeycomb-team", ""), ("x-honeycomb-dataset", "python-otlp"))
 )
 
 trace.set_tracer_provider(TracerProvider(resource=Resource({"service.name": "python-otlp", "service.version":"0.1"})))
 trace.get_tracer_provider().add_span_processor(
-    BatchExportSpanProcessor(otlp_exporter)
+    BatchSpanProcessor(otlp_exporter)
 )
 
 app = flask.Flask(__name__)
