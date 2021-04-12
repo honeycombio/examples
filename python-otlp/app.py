ecda7293acc6097cec364c01f7cff72d658af720
Shelby Spees <shelby@honeycomb.io>
2021-04-12 11:30:41 -0700

    Apply autopep8 formatting

--- python-otlp/app.py
+++ python-otlp/app.py
@@ -1,10 +1,9 @@
 import flask
 import requests
-
 from grpc import ssl_channel_credentials
-
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import \
+    OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.resources import Resource
@@ -14,7 +13,7 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 otlp_exporter = OTLPSpanExporter(
 	endpoint="api.honeycomb.io:443",
 	credentials=ssl_channel_credentials(),
-	headers=(("x-honeycomb-team", ""),("x-honeycomb-dataset","python-otlp"))
+    headers=(("x-honeycomb-team", ""), ("x-honeycomb-dataset", "python-otlp"))
 )
 
 trace.set_tracer_provider(TracerProvider(resource=Resource({"service.name": "python-otlp", "service.version":"0.1"})))
