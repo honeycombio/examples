e89f5ff9669bdaa130a6222b6e23d6112a3c432a
Vera Reynolds <verareynolds@honeycomb.io>
2021-08-16 14:57:50 -0600

    Update OTEL-JS example

--- node-otlp/tracing.js
+++ node-otlp/tracing.js
@@ -1,41 +1,38 @@
 "use strict";
 
-const grpc = require("@grpc/grpc-js");
-const { Resource } = require("@opentelemetry/resources");
-const { ResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ExpressInstrumentation } = require("@opentelemetry/instrumentation-express");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const { NodeTracerProvider } = require("@opentelemetry/node");
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
+const process = require('process');
+const { Metadata, credentials } = require("@grpc/grpc-js");
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 const { CollectorTraceExporter } = require("@opentelemetry/exporter-collector-grpc");
 
-const provider = new NodeTracerProvider({
-  resource: new Resource({
-    [ResourceAttributes.SERVICE_NAME]: 'node-otlp',
-  }),
-})
 
-const metadata = new grpc.Metadata();
-metadata.set('x-honeycomb-team', '<YOUR-APIKEY>');
-metadata.set('x-honeycomb-dataset', '<YOUR-DATASET>');
-
-provider.addSpanProcessor(
-  new SimpleSpanProcessor(
-    new CollectorTraceExporter({
-      url: 'grpc://api.honeycomb.io:443/',
-      credentials: grpc.credentials.createSsl(),
-      metadata
-    })
-  )
-);
-provider.register();
-
-registerInstrumentations({
-  instrumentations: [
-    HttpInstrumentation,
-    ExpressInstrumentation
-  ]
+const metadata = new Metadata()
+metadata.set('x-honeycomb-team', '<YOUR_API_KEY>');
+metadata.set('x-honeycomb-dataset', '<YOUR_DATASET>');
+const traceExporter = new CollectorTraceExporter({
+  url: 'grpc://api.honeycomb.io:443/',
+  credentials: credentials.createSsl(),
+  metadata
 });
 
-console.log("tracing initialized");
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'node-otlp',
+  }),
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()]
+});
+
+sdk.start()
+  .then(() => console.log('Tracing initialized'))
+  .catch((error) => console.log('Error initializing tracing', error));
+
+process.on('SIGTERM', () => {
+  sdk.shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
